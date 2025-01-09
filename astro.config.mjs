3f34cd2c4ffc66e33a5896528d29fcc8140cd518
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-01-09 16:01:49 -0500

    decap starter
    integrating decap starter
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,29 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({});
+
+import {defineConfig} from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import {SITE_URL} from "./src/data/config"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap()],
+  site: SITE_URL,
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "nord",
+      wrap: false,
+    },
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": new URL("./src/", import.meta.url).pathname,
+      },
+    },
+  },
+})
