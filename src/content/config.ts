2c250794b5479ff172c8b4d5a7ecbe62c49e39c5
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-02-25 16:47:55 -0500

    config ts file
    config ts file for blogs brought back in and working
    
    Co-Authored-By: Katayon Haghighat <100489705+KatayonH@users.noreply.github.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,25 @@
-// import {z, defineCollection} from "astro:content"
+import {z, defineCollection} from "astro:content"
 
-// const blogCollection = defineCollection({
-//   type: "content",
-//   schema: ({image}) =>
-//     z.object({
-//       // meta_title: z.string().optional(),
-//       // meta_description: z.string().optional(),
-//       title: z.string(),
-//       date: z.date(),
-//       // card_image: image(),
-//       blog_image: image(),
-//       // categories: z.array(z.string()),
-//       // tags: z.array(z.string()).optional(),
-//       // pinned: z.boolean().default(false),
-//       // preview: z.string().optional(),
-//       // cta_text: z.string().optional(),
-//       // cta_link: z.string().optional(),
-//       // color: z.string().optional(),
-//       content: z.string().optional(),
-//     }),
-// })
+const blogCollection = defineCollection({
+  type: "content",
+  schema: ({image}) =>
+    z.object({
+      // meta_title: z.string().optional(),
+      // meta_description: z.string().optional(),
+      title: z.string(),
+      date: z.date(),
+      // card_image: image(),
+      image: image(),
+      // categories: z.array(z.string()),
+      // tags: z.array(z.string()).optional(),
+      // pinned: z.boolean().default(false),
+      // preview: z.string().optional(),
+      // cta_text: z.string().optional(),
+      // cta_link: z.string().optional(),
+      // color: z.string().optional(),
+    }),
+})
 
-// export const collections = {
-//   blog: blogCollection,
-// }
+export const collections = {
+  blog: blogCollection,
+}
